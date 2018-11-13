d7b46bd3c0b7a701cb4b21387095390965153df4
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-13 12:22:24 +0800

    added ability to add new tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
   get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
