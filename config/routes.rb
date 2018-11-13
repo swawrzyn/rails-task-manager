2e8517134490f9f186963db413eb3865c4c851bf
Stefan Wawrzyn <swawrzyn@gmail.com>
2018-11-13 12:06:20 +0800

    added detailed task view with links to/from index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#all'
+  root 'tasks#index'
+  get 'tasks', to: 'tasks#index', as: :tasks
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
